600c770a50dccfd0fe05eec644401662816cd1e1
mintybubblegum <noa.trujillo@bosonit.com>
2023-05-22 09:26:25 +0200

    added last data structure exercise

--- 4.JAVASCRIPT/data-structure.js
+++ 4.JAVASCRIPT/data-structure.js
@@ -27,3 +27,37 @@ return foods[fruitValue];
 console.log(checkUnit("apples"));
 console.log(checkUnit("bananas"));
 console.log(checkUnit("strawberries"));
+
+//---------------------------------
+
+//Echa un vistazo al objeto que hemos proporcionado en el editor de código. El objeto user contiene tres claves. La clave data contiene cinco claves, una de las cuales contiene un arreglo de friends. A partir de esto, puedes ver lo flexibles que son los objetos como estructuras de datos. Hemos empezado a escribir una función addFriend. Termina de escribirla para que tome un objeto user y agregue el nombre del argumento friend al arreglo almacenado en user.data.friends y devuelva ese arreglo.
+
+let user = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+        username: 'kennethCodesAllDay',
+        joinDate: 'March 26, 2016',
+        organization: 'freeCodeCamp',
+        friends: [
+        'Sam',
+        'Kira',
+        'Tomo'
+        ],
+        location: {
+            city: 'San Francisco',
+            state: 'CA',
+            country: 'USA'
+        }
+    }
+};
+
+function addFriend(userObj, friend) {
+    // Cambia solo el código debajo de esta línea
+    userObj = user;
+    userObj.data.friends.push("Pete");
+    return user.data.friends
+    // Cambia solo el código encima de esta línea
+}
+
+console.log(addFriend(user, 'Pete'));
