689b1e8e0dc59347bf2c89bb6c24a4281aea9302
mintybubblegum <noa.trujillo@bosonit.com>
2023-05-24 16:24:34 +0200

    added more exercises in object literals file

--- 5.UDEMY/5.1.JS/literal-objects.js
+++ 5.UDEMY/5.1.JS/literal-objects.js
@@ -21,3 +21,19 @@ let personaje = {
   console.log('Lat', personaje.coords.lat); //imprime latitud a través de concatenacion de dot notation
   console.log ('No. trajes', personaje.trajes.length); //calcula numero de trajes
   console.log ('último traje', personaje.trajes[personaje.trajes.length - 1]); //imprime sólo último traje
+
+//más detalles
+
+delete personaje.edad;
+console.log(personaje);
+
+personaje.casado =  true;
+
+Object.freeze(personaje);//para no poder cambiar los valores del objeto y bloquearlo. 
+personaje.casado = false;//lanza error porque es una propiedad directa del objeto bloqueado
+personaje.direccion.ubicacion = 'Costa Rica';
+console.log(personaje);//Lo que si se pueden modificar son los objetos de objetos
+
+const propiedades = Object.getOwnPropertyNames(personaje);//imprime solo las propiedades del objeto
+const valores = Object.values(personaje);//imprime solo los valores del objeto
+console.log({propiedades, valores});
