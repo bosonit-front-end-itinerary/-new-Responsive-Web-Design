dba59a7e60ff7a14b602382d092b452cfd86c991
mintybubblegum <noa.trujillo@bosonit.com>
2023-05-26 10:01:44 +0200

    getters y setters added

--- 5.UDEMY/5.1.JS/classes/class.js
+++ 5.UDEMY/5.1.JS/classes/class.js
@@ -1,19 +1,31 @@
-
 //constructor: todo lo necesario para crear futuras nuevas instancias. metodo que se va a ejecutar cada vez que se crea nueva instancia. sintaxis mucho más limpia
 class Persona {
 
-    //estas son las propiedades que vamos a usar en nuestro constructor
+//estas son las propiedades que vamos a usar en nuestro constructor
     nombre = ''; 
     codigo = '';
     frase = '';
+    comida = '';
     
-    //inicializar los valores de las propiedades de arriba dentro de los paréntesis de nuestro constructor
+//inicializar los valores de las propiedades de arriba dentro de los paréntesis de nuestro constructor
     constructor(nombre, codigo, frase) {
       //los valores definidos abajo son contenedores para ser utilizados en cada instancia de clases
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
     }
+
+
+//GETTERS y SETTERS
+    //inicializar con la palabra set y luego el nombre que elijamos que empiece por set también, para que al instanciarlo del mismo modo que un método veamos que es un set. siempre tenemos que rellenar el método (solo un argumento)
+    set setComidaFavorita(comida){
+    this.comida = comida;
+}
+
+//aconsejable que en los GETS añadamos un return
+    get getComidaFavorita(){
+    return `La comida favorita de ${this.nombre} es ${this.comida}`;
+}
     
     quienSoy(){
         console.log(`Soy ${this.nombre} y mi trabajo es ${this.codigo}`);
@@ -27,11 +39,9 @@ class Persona {
 //instanciar nueva instancia a partir de nuestra clase Persona
 const spiderman = new Persona('Noa', 'Web developer', 'I love you darling'); 
 const miles = new Persona('Miles', 'plumber', 'I love you to the moon and back');
-console.log(spiderman);
-console.log(miles);
-
-spiderman.quienSoy();
-miles.quienSoy();
 
 spiderman.miFrase();
-miles.miFrase();
+
+spiderman.setComidaFavorita = 'El pie de cereza de la tia May';
+console.log(spiderman);
+console.log(spiderman.getComidaFavorita);
