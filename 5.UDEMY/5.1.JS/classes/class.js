bc18cb6f7bf13ddd2b7a4f57e5baaafdd84e9f98
mintybubblegum <noa.trujillo@bosonit.com>
2023-05-26 11:16:11 +0200

    added static property, get and method

--- 5.UDEMY/5.1.JS/classes/class.js
+++ 5.UDEMY/5.1.JS/classes/class.js
@@ -1,6 +1,18 @@
 //constructor: todo lo necesario para crear futuras nuevas instancias. metodo que se va a ejecutar cada vez que se crea nueva instancia. sintaxis mucho más limpia
 class Persona {
 
+      //propiedad estática
+    static _conteo = 0;
+    //get como metodo estatico
+    static get conteo() {
+        return Persona._conteo + ' instancias';
+    }
+    //metodo estático
+    static mensaje() {
+        console.log(this.nombre);
+        console.log('Hola coders, soy un método estático');
+    }
+
 //estas son las propiedades que vamos a usar en nuestro constructor
     nombre = ''; 
     codigo = '';
@@ -13,6 +25,8 @@ class Persona {
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
+
+        Persona._conteo++;
     }
 
 
@@ -45,3 +59,8 @@ spiderman.miFrase();
 spiderman.setComidaFavorita = 'El pie de cereza de la tia May';
 console.log(spiderman);
 console.log(spiderman.getComidaFavorita);
+
+//para contar cuantas instancias tenemos de nuestra clase
+console.log('Conteo estático',Persona._conteo);//imprimir propiedad estatica
+console.log(Persona.conteo);//imprimer get estatico
+Persona.mensaje();//imprimir metodo estático
